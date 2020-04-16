653c5fe183ed44353a57f3745e1065fd5e74d264
Marius Rieder <marius.rieder@scs.ch>
2020-04-16 06:55:04 +0200

    Set encoding to cp437 to enable non ascii characters on task names.

--- agents/windows/plugins/win_scheduled_task.ps1
+++ agents/windows/plugins/win_scheduled_task.ps1
@@ -1,4 +1,4 @@
-Write-Host '<<<win_scheduled_task:sep(9)>>>'
+﻿Write-Host '<<<win_scheduled_task:sep(9):encoding(cp437)>>>'
 Get-ScheduledTask | Get-ScheduledTaskInfo | ForEach-Object {
     Write-Host "$($_.TaskPath)$($_.TaskName)`t$($_.LastTaskResult)`t$($_.LastRunTime)`t$($_.NextRunTime)"
 }
